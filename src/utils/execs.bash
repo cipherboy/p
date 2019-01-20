3dbb8ad70e9b5e3f31feaa6ddc6078965aa86b9d
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-01-20 10:49:27 -0500

    Fix pass execution when --password-store-dir is used
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- src/utils/execs.bash
+++ src/utils/execs.bash
@@ -1,6 +1,22 @@
 # Execute the pass command with the given arguments.
 function __pass() {
+    # Note that we need to handle PASSWORD_STORE_DIR here: since _p_pass_dir
+    # is the canonical variable (storing first PASSWORD_STORE_DIR, then any
+    # command line flags passed to `p`), if it differs, we should preserve the
+    # old PASSWORD_STORE_DIR value.
+
+    if [ -z "${PASSWORD_STORE_DIR+x}" ]; then
+        local old_value="$PASSWORD_STORE_DIR"
+    fi
+
+    export PASSWORD_STORE_DIR="$_p_pass_dir"
     $_p_pass_path "$@"
+
+    if [ -z "${old_value+x}" ]; then
+        export PASSWORD_STORE_DIR="$old_value"
+    else
+        unset PASSWORD_STORE_DIR
+    fi
 }
 
 # Execute the jq command with the given arguments.
